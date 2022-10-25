44b63d6e014e87dbad64b6e695bb157fced8839b
tomas <tomasmessiga@uca.edu.ar>
2022-10-25 18:02:23 -0300

    TDD iteration

--- test/testPrimeraParte.test.js
+++ test/testPrimeraParte.test.js
@@ -39,13 +39,13 @@ test("Prueba tablero al encenderse",()=>{
     tableroArmado.agragarLuces(1,1);
     const tableroUtil=tableroArmado.prueba();
     console.log(tableroUtil);
-    expect(object.tableroUtil.name=="Tablero").toBe(true);
+    expect(Object.tableroUtil.name=="Tablero").toBe(true);
 })
 test("Prueba tablero al encenderse (no funciona el tablero)",()=>{
     const tableroArmado=new Tablero();
     const tableroUtil=tableroArmado.prueba();
     console.log(tableroUtil);
-    expect(object.tableroUtil.name=="Tablero").toBe(true);
+    expect(Object.tableroUtil.name=="Tablero").toBe(true);
 })
 
 
