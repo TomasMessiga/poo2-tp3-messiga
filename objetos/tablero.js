974322f9a75c4bee58116b6720561b72f9e77dba
tomas <tomasmessiga@uca.edu.ar>
2022-10-24 22:38:13 -0300

    TDD iteration

--- objetos/tablero.js
+++ objetos/tablero.js
@@ -1,5 +1,40 @@
-function Tablero(luces){
-    this.luces=luces;
+
+/** indicaciones
+ *  0 (luz inactiva)
+ *  1 (luz activa)
+ */
+ 
+
+function Tablero(){
+    this.luces=new Array();
+
+    this.esTablero=function(){
+        if (this.lucesTotales()==0){
+            return false;
+        }
+        return true;
+    }
+    this.agragarLuces=function(filas,columnas){
+        for (let i=0;i<filas;i++){
+            let filaAuxiliar=new Array();
+            for (let f=0;f<columnas;f++){
+                filaAuxiliar.push(0);
+            }
+            this.luces.push(filaAuxiliar);
+        }
+    }
+    this.lucesTotales=function(){
+        let cont=0;
+        this.luces.forEach(element => {
+            this.luces.forEach(element => {
+                cont++;
+            });       
+        });
+        return cont;
+    }
+
+
 }
+
 
 module.exports=Tablero;
--- test/testPrimeraParte.test.js
+++ test/testPrimeraParte.test.js
@@ -1,33 +1,27 @@
-const TableroInactivo=require("../objetos/tableroInactivo.js");
-const Tablero=require("../objetos/tablero.js");
+const tablero=require("../objetos/tablero.js");
 
 test("Prueba tablero: incorporar luces (cant 1)",()=>{
-    const tablero=new TableroInactivo();
+    const tablero=new Tablero();
     tablero.agragarLuces(1,1);
     expect(tablero.lucesTotales()).toBe(1);
 })
 test("Prueba tablero: incorporar luces (cant 4)",()=>{
-    const tablero=new TableroInactivo();
+    const tablero=new Tablero();
     tablero.agragarLuces(2,2);
     expect(tablero.lucesTotales()).toBe(4);
 })
 test("Prueba tablero: incorporar luces (cant 16)",()=>{
-    const tablero=new TableroInactivo();
+    const tablero=new Tablero();
     tablero.agragarLuces(4,4);
     expect(tablero.lucesTotales()).toBe(16);
 })
 test("Prueba tablero: incorporar luces (cant 10000)",()=>{
-    const tablero=new TableroInactivo();
+    const tablero=new Tablero();
     tablero.agragarLuces(100,100);
     expect(tablero.lucesTotales()).toBe(10000);
 })
 
-test("Prueba tablero: incorporar luces (cant 10000)",()=>{
-    const tableroArmado=new TableroInactivo();
-    tableroArmado.agragarLuces(100,100);
-    const tablero=tableroArmado.encender();
-    expect(tablero==='Tablero').toBe(true);
-})
+
 
 
 
