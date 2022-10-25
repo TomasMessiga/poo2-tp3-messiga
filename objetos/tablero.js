d6e25c1f3dbf74081e61550af6ebe16c8cc5b44f
tomas <tomasmessiga@uca.edu.ar>
2022-10-25 15:42:46 -0300

    TDD iteration

--- objetos/tablero.js
+++ objetos/tablero.js
@@ -76,6 +76,7 @@ function Tablero(){
         }
     }
     this.apagar=function(cord1,cord2){
+        numeroEnRangoNumericoPositivo(cord1,cord2)
         for (let i=0;i<this.luces.length;i++){
             for (let j=0;j<this.luces[i].length;j++){
                 if (i>=cord1[0] && i<=cord2[0] && j>=cord1[1] && j<=cord2[1]){
