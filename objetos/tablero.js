c759311729ba893c7e97b0a91d796be80e47c9a2
tomas <tomasmessiga@uca.edu.ar>
2022-10-24 22:47:34 -0300

    TDD iteration

--- objetos/tablero.js
+++ objetos/tablero.js
@@ -35,7 +35,7 @@ function Tablero(){
     this.encender=function(){
         let tableroUtil=new Tablero();
         tableroUtil.luces=this.luces;
-        if (this.lucesTotales==0){
+        if (this.lucesTotales()==0){
             return undefined;
         }
         return tableroUtil;
