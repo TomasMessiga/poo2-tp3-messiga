ae7bf4b8d2919903ecc20f6090f073174dfef514
tomas <tomasmessiga@uca.edu.ar>
2022-10-25 19:01:42 -0300

    TDD iteration

---
+++ objetos/tableroLED.js
@@ -0,0 +1,119 @@
+
+/** indicaciones
+ *  0 (luz inactiva)
+ *  1 (luz activa)
+ */
+ 
+ const numeroPositivo=function(numero){
+    if (numero<=0){
+        throw ("Error en las dimensiones establecidas")
+    }
+}
+const numeroNoNegativo=function(numero){
+    if (numero<0){
+        throw ("Error en las dimensiones establecidas")
+    }
+}
+const numeroEnRangoNumericoPositivo=function(cord1,cord2){
+    numeroNoNegativo(cord1[0])
+    numeroNoNegativo(cord1[1])
+    numeroNoNegativo(cord2[0])
+    numeroNoNegativo(cord2[1])
+    if (cord2[0]<cord1[0] || cord2[1]<cord1[1] ){
+        throw("Error en el rango establecido")
+    }
+}
+
+const estadoOpuesto={
+    1:0,
+    0:1,
+}
+
+const TableroNull =function(){
+    this.luces=undefined;    
+    this.encender=function(cord1,cord2){throw("El tablero no puede encender luces")}
+    this.apagar=function(cord1,cord2){throw("No funcionan las luces, por ende todas las luces estan apagadas")}
+    this.cambiar=function(cord1,cord2){throw("No se pueden alterar el estado de las luces")}
+    this.lucesEncendidas=()=>{return 0}
+}
+
+
+
+function Tablero(){
+this.luces=undefined;
+
+this.agragarLuces=function(filas,columnas){
+    numeroPositivo(filas)
+    numeroPositivo(columnas)
+    this.luces=new Array();
+    for (let i=0;i<filas;i++){
+        let filaAuxiliar=new Array();
+        for (let f=0;f<columnas;f++){
+            filaAuxiliar.push(0);
+        }
+        this.luces.push(filaAuxiliar);
+    }
+}
+
+this.lucesTotales=function(){
+    let cont=0;
+    this.luces.forEach(aux1 => {
+        aux1.forEach(aux2 => {
+            cont++;
+        });       
+    });
+    return cont;
+}  
+
+
+this.prueba=function(){
+    if (this.luces==undefined){
+        return new TableroNull();
+    }      
+    let tableroUtil=new Tablero();
+    tableroUtil.luces=this.luces;
+    return tableroUtil;
+}
+
+
+this.encender=function(cord1,cord2){
+    numeroEnRangoNumericoPositivo(cord1,cord2)
+    for (let i=cord1[0];i<=cord2[0];i++){
+        for (let j=cord1[1];j<=cord2[1];j++){
+                this.luces[i][j]=1;
+        }
+    }
+
+}
+
+this.apagar=function(cord1,cord2){
+    numeroEnRangoNumericoPositivo(cord1,cord2)
+    for (let i=cord1[0];i<=cord2[0];i++){
+        for (let j=cord1[1];j<=cord2[1];j++){
+                this.luces[i][j]=0;
+        }
+    } 
+}
+this.cambiar=function(cord1,cord2){
+    
+    numeroEnRangoNumericoPositivo(cord1,cord2)
+    for (let i=cord1[0];i<=cord2[0];i++){
+        for (let j=cord1[1];j<=cord2[1];j++){
+            this.luces[i][j]=estadoOpuesto[this.luces[i][j]];
+        }
+    } 
+}
+this.lucesEncendidas=()=>{
+    let cont=0;
+    const filtrado=function(luz){return luz==1;};
+    this.luces.forEach(aux1 => {
+        cont+=(aux1.filter(filtrado)).length;   
+    });
+    return cont;
+}
+
+}
+
+
+
+module.exports=Tablero;
---
+++ test/testSegundaParte.test.js
@@ -0,0 +1,197 @@
+const Tablero=require("../objetos/tableroLED.js");
+
+const contadorDeLucesActivas=(arreglo)=>{
+    var cont=0
+    arreglo.forEach(aux => {
+        aux.forEach(element => {
+            if (element==1){
+                cont++;
+            }
+        });
+    });
+    return cont;
+}
+
+test("Prueba tablero: incorporar luces (cant 1)",()=>{
+    const tablero=new Tablero();
+    tablero.agragarLuces(1,1);
+    expect(tablero.lucesTotales()).toBe(1);
+})
+test("Prueba tablero: incorporar luces (cant 4)",()=>{
+    const tablero=new Tablero();
+    tablero.agragarLuces(2,2);
+    expect(tablero.lucesTotales()).toBe(4);
+})
+test("Prueba tablero: incorporar luces (cant 16)",()=>{
+    const tablero=new Tablero();
+    tablero.agragarLuces(4,4);
+    expect(tablero.lucesTotales()).toBe(16);
+})
+test("Prueba tablero: incorporar luces (cant 10000)",()=>{
+    const tablero=new Tablero();
+    tablero.agragarLuces(100,100);
+    expect(tablero.lucesTotales()).toBe(10000);
+})
+
+
+test("Prueba tablero al encenderse",()=>{
+    const tableroArmado=new Tablero();
+    tableroArmado.agragarLuces(1,1);
+    const tableroUtil=tableroArmado.prueba();
+    const proto = Object.getPrototypeOf;
+    expect(proto(tableroUtil).constructor === Tablero).toBe(true);
+})
+test("Prueba tablero al encenderse (no funciona el tablero)",()=>{
+    const tableroArmado=new Tablero();
+    const tableroUtil=tableroArmado.prueba();
+    const proto = Object.getPrototypeOf;
+    expect(proto(tableroUtil).constructor === Tablero).toBe(false);
+})
+
+
+test("Prueba encender tablero 1",()=>{
+    const tableroArmado=new Tablero();
+    tableroArmado.agragarLuces(100,100);
+    const tablero=tableroArmado.prueba();
+    tablero.encender([0,0],[0,0]);
+    expect(tablero.luces[0][0]).toBe(1);
+})
+/
+test("Prueba encender tablero 2",()=>{
+    const tableroArmado=new Tablero();
+    tableroArmado.agragarLuces(100,100);
+    const tablero=tableroArmado.prueba();
+    tablero.encender([0,0],[2,2]);
+    expect(contadorDeLucesActivas(tablero.luces)).toBe(9);
+})
+test("Prueba encender tablero 3",()=>{
+    const tableroArmado=new Tablero();
+    tableroArmado.agragarLuces(100,100);
+    const tablero=tableroArmado.prueba();
+    tablero.encender([1,2],[3,2]);
+    expect(contadorDeLucesActivas(tablero.luces)).toBe(3);
+})
+test("Prueba encender tablero 4",()=>{
+    const tableroArmado=new Tablero();
+    tableroArmado.agragarLuces(100,100);
+    const tablero=tableroArmado.prueba();
+    tablero.encender([0,0],[99,99]);
+    expect(contadorDeLucesActivas(tablero.luces)).toBe(100*100);
+})
+
+
+
+test("Prueba apagar tablero 1",()=>{
+    const tableroArmado=new Tablero();
+    tableroArmado.agragarLuces(100,100);
+    const tablero=tableroArmado.prueba();
+    tablero.encender([0,0],[99,99]);
+    tablero.apagar([0,0],[0,0]);
+    expect(contadorDeLucesActivas(tablero.luces)).toBe(10000-1);
+})
+test("Prueba apagar tablero 2",()=>{
+    const tableroArmado=new Tablero();
+    tableroArmado.agragarLuces(100,100);
+    const tablero=tableroArmado.prueba();
+    tablero.encender([0,0],[99,99]);
+    tablero.apagar([0,0],[2,2]);
+    expect(contadorDeLucesActivas(tablero.luces)).toBe(10000-9);
+})
+test("Prueba apagar tablero 3",()=>{
+    const tableroArmado=new Tablero();
+    tableroArmado.agragarLuces(100,100);
+    const tablero=tableroArmado.prueba();
+    tablero.encender([0,0],[99,99]);
+    tablero.apagar([0,0],[99,99]);
+    expect(contadorDeLucesActivas(tablero.luces)).toBe(0);
+})
+
+
+test("Prueba cambiar tablero 1",()=>{
+    const tableroArmado=new Tablero();
+    tableroArmado.agragarLuces(100,100);
+    const tablero=tableroArmado.prueba();
+    tablero.encender([0,0],[0,0]);
+    tablero.cambiar([0,0],[0,1]);
+    expect(tablero.luces[0][0]+tablero.luces[0][1]).toBe(1);
+})
+test("Prueba cambiar tablero 2",()=>{
+    const tableroArmado=new Tablero();
+    tableroArmado.agragarLuces(100,100);
+    const tablero=tableroArmado.prueba();
+    tablero.encender([0,0],[2,2]);
+    tablero.cambiar([0,0],[2,3]);
+    expect(contadorDeLucesActivas(tablero.luces)).toBe(3);
+})
+test("Prueba cambiar tablero 3",()=>{
+    const tableroArmado=new Tablero();
+    tableroArmado.agragarLuces(100,100);
+    const tablero=tableroArmado.prueba();
+    tablero.encender([0,0],[99,99]);
+    tablero.cambiar([0,0],[99,99]);
+    expect(contadorDeLucesActivas(tablero.luces)).toBe(0);
+})
+
+
+
+test("Prueba tablero (luces encendidas) 1",()=>{
+    const tableroArmado=new Tablero();
+    tableroArmado.agragarLuces(100,100);
+    const tablero=tableroArmado.prueba();
+    expect(tablero.lucesEncendidas()).toBe(0);
+})
+test("Prueba tablero (luces encendidas) 2",()=>{
+    const tableroArmado=new Tablero();
+    tableroArmado.agragarLuces(100,100);
+    const tablero=tableroArmado.prueba();
+    tablero.encender([0,0],[0,0]);
+    expect(tablero.lucesEncendidas()).toBe(1);
+})
+test("Prueba tablero (luces encendidas) 3",()=>{
+    const tableroArmado=new Tablero();
+    tableroArmado.agragarLuces(100,100);
+    const tablero=tableroArmado.prueba();
+    tablero.encender([0,0],[2,2]);
+    expect(tablero.lucesEncendidas()).toBe(9);
+})
+test("Prueba tablero (luces encendidas) 4",()=>{
+    const tableroArmado=new Tablero();
+    tableroArmado.agragarLuces(100,100);
+    const tablero=tableroArmado.prueba();
+    tablero.encender([0,0],[99,99]);
+    tablero.apagar([6,6],[6,7]);
+ //   expect(tablero.lucesEncendidas()).toBe(10000-2);
+})
+
+
+
+test("Prueba tablero (error en dimesiones)",()=>{
+    const tableroArmado=new Tablero();
+    try{
+        tableroArmado.agragarLuces(-1,-1);
+    } catch (e){
+        expect(e).toBe("Error en las dimensiones establecidas");
+    }
+})
+test("Prueba tablero luces (error en dimesiones)",()=>{
+    const tableroArmado=new Tablero();
+    tableroArmado.agragarLuces(100,100);
+    const tablero=tableroArmado.prueba();
+    try{
+        tablero.cambiar([0,0],[-1,-1]);
+        throw("")
+    } catch (e){
+        expect(e).toBe("Error en las dimensiones establecidas");
+    }
+})
+test("Prueba tablero luces (error en el rango)",()=>{
+    const tableroArmado=new Tablero();
+    tableroArmado.agragarLuces(100,100);
+    const tablero=tableroArmado.prueba();
+    try{
+        tablero.cambiar([3,3],[2,2]);
+        throw("")
+    } catch (e){
+        expect(e).toBe("Error en el rango establecido");
+    }
+})
