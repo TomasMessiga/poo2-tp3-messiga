6b9c58f9d92c4ed20f277d2b2c1327539069b271
tomas <tomasmessiga@uca.edu.ar>
2022-10-25 19:41:59 -0300

    TDD iteration

--- test/testSegundaParte.test.js
+++ test/testSegundaParte.test.js
@@ -102,8 +102,9 @@ test("Prueba apagar tablero 3",()=>{
     tableroArmado.agragarLuces(2,2);
     const tablero=tableroArmado.prueba();
     tablero.encender([0,0],[0,0]);
-    tablero.apagar([0,0],[1,1]);
-    expect(contadorDeLucesActivas(tablero.luces)).toBe(0);
+    tablero.encender([0,0],[0,1]);
+    tablero.apagar([0,0],[0,0]);
+    expect(contadorDeLucesActivas(tablero.luces)).toBe(1);
 })
 
 
